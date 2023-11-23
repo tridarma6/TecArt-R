bc672a69872e5a199a91cd0745f75024c2b7adfe
tridarma6 <triidarma06@gmail.com>
2023-11-23 15:14:14 +0800

    Menambahkan keseluruhan file

---
+++ script.js
@@ -0,0 +1,45 @@
+const containerEl = document.getElementById("container");
+const leftEl = document.getElementById("left");
+const rightEl = document.getElementById("right");
+
+const userEmailEl = document.getElementById("user-email");
+const invalidEmailEl = document.getElementById("invalid-email");
+const emailInput = document.getElementById("email");
+const submitBtnEl = document.getElementById("submit-btn");
+
+const confirmedMessageEl = document.getElementById("confirmed-message");
+const dismissMessageEl = document.getElementById("dismiss-message");
+
+function formSucess() {
+  confirmedMessageEl.classList.add("active");
+  containerEl.classList.add("success");
+  leftEl.style.display = "none";
+  rightEl.style.display = "none";
+}
+function validasiEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+submitBtnEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  const email = emailInput.value.trim();
+
+  if (validasiEmail(email)) {
+    formSucess();
+    userEmailEl.innerText = email;
+    emailInput.value = "";
+
+    invalidEmailEl.classList.remove("active");
+    emailInput.classList.remove("active");
+  } else {
+    invalidEmailEl.classList.add("active");
+    emailInput.classList.add("active");
+  }
+});
+dismissMessageEl.addEventListener("click", () => {
+  leftEl.style.display = "block";
+  rightEl.style.display = "block";
+  containerEl.classList.remove("success");
+  confirmedMessageEl.classList.remove("active");
+});
